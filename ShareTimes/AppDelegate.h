2679d76b71e2aa618d4c35e4cca23d61bc09aa54
wzhen22 <qwaszx375204@126.com>
2014-06-05 01:46:37 -0700

    Initial Commit

---
+++ ShareTimes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ShareTimes
+//
+//  Created by 传晟 on 14-6-5.
+//  Copyright (c) 2014年 传晟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
